bac4cd8caf9c0a321f464c8adee269b250086d17
Steve Cox <scox@renci.org>
2013-11-13 13:22:41 -0500

    socket io

--- public/javascripts/messageController.js
+++ public/javascripts/messageController.js
@@ -6,43 +6,75 @@
  * - retrieves and persists the model via the todoStorage service
  * - exposes the model to the template and provides event handlers
  */
-instrumentationApp.controller ('eventController', function InlineEditorController ($scope, dataService){
-
+instrumentationApp.controller ('eventController', function MessageController ($scope, dataService, socket){
     $scope.showtooltip = false;
-    $scope.value = 'Edit me.';
-    $scope.items = [ ];
-    $scope.maxItemsLength = 5;
     $scope.index = 0;
+    $scope.value = '';
+    $scope.messages = [ ];
+    $scope.maxItemsLength = 5;
+    $scope.users = [];
 
-    $scope.hideTooltip = function(){
+    $scope.addFile = function () {
+	var id = $scope.index++;
+	var message = {
+	    id       : id,
+	    name     : $scope.name,
+	    date     : new Date (),
+	    filename : [ 'file-', id ].join (''),
+	    content  : $scope.value
+	};
+	dataService.addFile (message);
+	$scope.sendMessage (message);
+    };
 
-	// When a model is changed, the view will be automatically
-	// updated by by AngularJS. In this case it will hide the tooltip.
-
-	$scope.showtooltip = false;
-    }
-
-    $scope.toggleTooltip = function(e){
-	e.stopPropagation();
-	$scope.showtooltip = !$scope.showtooltip;
-    }
-
-    $scope.go = function () {
-
-	dataService.addFile ({ filename : 'fn', content : 'data....' });
-
-	$scope.index++;
-        $scope.items.push ({
-	    id      : $scope.index,
-            data    : new Date (),
-            details : 'dets[' + $scope.index + ']'
-        });
-
-	if ($scope.items.length > $scope.maxItemsLength) {
+    $scope.addMessage = function (message) {
+	$scope.messages.push (message);
+	if ($scope.messages.length > $scope.maxItemsLength) {
 	    var indexToRemove = 0;
 	    var numberToRemove = 1;
-	    $scope.items.splice (indexToRemove, numberToRemove);
+	    $scope.messages.splice (indexToRemove, numberToRemove);
         }
-    }
+    };
+
+    // Socket listeners
+    // ================
+
+    socket.on('init', function (data) {
+	$scope.name = "awesome";
+    });
+
+    socket.on('send:message', function (message) {
+	console.log (message);
+	$scope.addMessage (message);
+    });
+
+    socket.on('user:join', function (data) {
+	$scope.users.push (data)
+    });
+
+    // add a message to the conversation when a user disconnects or leaves the room
+    socket.on('user:left', function (data) {
+	var i, user;
+	for (i = 0; i < $scope.users.length; i++) {
+	    user = $scope.users[i];
+	    if (user === data) {
+		$scope.users.splice (i, 1);
+		break;
+	    }
+	}
+    });
+
+    // Methods published to the scope
+    // ==============================
+
+    $scope.sendMessage = function (message) {
+	socket.emit('send:message', message);
+
+	// add the message to our model locally
+	$scope.addMessage (message);
+
+	// clear message box
+	$scope.value = '';
+    };
 
 });
---
+++ routes/socket.js
@@ -0,0 +1,40 @@
+var socket = require ('socket.io');
+var core = require ('../lib/core');
+
+var messageDB = {
+    messages : [
+	{ id : 1, text : "a" },
+	{ id : 2, text : "b" },
+	{ id : 3, text : "c" },
+	{ id : 4, text : "d" }
+    ]
+};
+
+// export function for listening to the socket
+module.exports = function (socket) {
+    var name = "roger";
+
+    // send the new user their name and a list of users
+    console.log ("sending message db on init");
+    socket.emit('init', messageDB);
+
+    // notify other clients that a new user has joined
+    socket.broadcast.emit('user:join', {
+	name: name
+    });
+
+    // broadcast a user's message to other users
+    socket.on('send:message', function (data) {
+	console.log ("---------------------> " + core.util.inspect (data));
+	socket.broadcast.emit('send:message', data);
+    });
+
+    // clean up when a user leaves, and broadcast it to other users
+    socket.on('disconnect', function () {
+	socket.broadcast.emit('user:left', {
+	    name: name
+	});
+    });
+    
+};
+module.exports.socketIO = socket;
